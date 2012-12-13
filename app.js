885823827db240d8751aba71bfc56b7602bf620f
Dan Silver <dannysilver3@gmail.com>
2012-12-12 22:19:24 -0600

    One more try...

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ passport.deserializeUser(function(obj, done) {
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
-var config;
+var config = {};
 if (!process.env.database) {
 	config = require('./config');
 }
